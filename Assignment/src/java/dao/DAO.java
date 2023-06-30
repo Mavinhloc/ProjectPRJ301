342e61ecc53f329f7f58eb0979924611e766c9a2
Mavinhloc <133078359+Mavinhloc@users.noreply.github.com>
2023-07-01 00:06:01 +0700

    funtion getAllProduct

--- Assignment/src/java/dao/DAO.java
+++ Assignment/src/java/dao/DAO.java
@@ -4,6 +4,7 @@
  */
 package dao;
 
+import context.DBContext;
 import entity.Product;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -18,13 +19,27 @@ import java.util.List;
 public class DAO {
 
     Connection conn = null;
-    PreparedStatement st = null;
+    PreparedStatement ps = null;
     ResultSet rs = null;
     
     public List<Product> getAllProduct(){
         List<Product> list = new ArrayList<>();
-        String query = "";
-        
+        String query = "select * from product";
+        try {
+            conn = new DBContext().getConnection();//ket noi SQL
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(5)
+                ));
+            }
+        } catch (Exception e) {
+        }
         return list;
     }
 
